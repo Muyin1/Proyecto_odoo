ba21b2c8852b900aae094ecc02529d60dbf4ba93
Muyin <burnettnicolas0@gmail.com>
2025-06-05 11:47:56 -0300

    Arreglos , correcion de menu, añado aparte security para que el usuario pueda acceder

--- taller_registro_vehiculo/__manifest__.py
+++ taller_registro_vehiculo/__manifest__.py
@@ -5,7 +5,7 @@
     'category' : 'Taller',
     'author' : 'Burnet Mauricio Nicolas',
     'depends' : ['base','contacts'],
-    'data' : ['views/taller_vehiculo_views.xml', 'views/taller_menu.xml'],
+    'data' : ['views/taller_vehiculo_views.xml', 'views/taller_menu.xml','security/ir.model.access.csv'],
     'installable' : True,
     'applications' : True,
 }
