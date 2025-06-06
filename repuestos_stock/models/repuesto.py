73d6073bd19a0174447be700517e7e23a122467f
Muyin <burnettnicolas0@gmail.com>
2025-06-06 16:54:01 -0300

    cambiando, integer por Integer , y agregando motor a cehiculo views

--- repuestos_stock/models/repuesto.py
+++ repuestos_stock/models/repuesto.py
@@ -9,5 +9,5 @@ class Repuesto(models.Model):
     codigo_oem = fields.Char(string='Codigo OEM')
     vehiculo_ids = fields.Many2many('repuestos_stock.vehiculo', string='Vehículos compatibles')
     descripcion = fields.Char(string='Descripcion')
-    cantidad = fields.integer(string='Cantidad en stock', default=0)
+    cantidad = fields.Integer(string='Cantidad en stock', default=0)
     precio_venta = fields.Float(string='Precio de venta')
