94129d90a300f5edee082869f1ce6c3506ea0dc6
Muyin <burnettnicolas0@gmail.com>
2025-06-12 17:59:23 -0300

    actualizacion grande, voy a crear categorias en vez de crear un item comerciable osea template de repuesto en general, entonces eso me va a permitir cargar todo desde otro apartado y verlo en el apartado normal del template

---
+++ repuestos_stock/models/inyector.py
@@ -0,0 +1,24 @@
+from odoo import models, fields
+
+class Inyector(models.Model):
+    _name = 'repuestos_stock.inyector'
+    _description = "inyector"
+    _inherit = "product.template"
+
+    #campos especificos del inyector
+    insulating_color = fields.Char(string = "Color Aislante")
+    injection_type = fields.Selection([
+        ('monopunto','Monopunto')
+        ('multipunto','Multipunto'),
+    ], string="tipo de Inyeccion")
+
+    #campos comunes a todos  los repuestos
+    codigo_repuesto = fields.Char(string="Codigo de Repuesto", required=True, copy=False)
+    codigo_oem = fields.Char(string="Coigo OEM")
+    marca_id = fields.Many2one('repuestos_stock.marca', String ="Marca")
+    vehiculo_ids = fields.Many2many('repuestos_stock.vehiculo', string='Vehiculos Compatibles')
+
+    _sql_constraints = [
+        ('unique_codigo_repuesto','UNIQUE(codigo_repuesto)',"El codigo del repuesto debe ser unico")
+    ]
+
--- repuestos_stock/models/tipo.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class TipoRepuesto(models.Model):
-    _name = 'repuestos_stock.tipo'
-    _description = 'Tipo de Repuesto'
-
-    name = fields.Char(string='Nombre del Tipo', required=True, translate=True)
-    descripcion = fields.Text(string='Descripción')
