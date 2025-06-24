dfbca17fb397b5395ab0401de048d9bc4b5d5fe7
Muyin <burnettnicolas0@gmail.com>
2025-06-24 11:50:06 -0300

    declarando lo que tuiene que cargar el programa a la hora de iniciar, osea procesoes web

--- repuestos_stock/static/src/js/repuesto_form.js
+++ repuestos_stock/static/src/js/repuesto_form.js
@@ -1,32 +1,36 @@
 
-odoo.define('repuestos_stock.repuesto_form', function (require) {
-    'use strict';
+odoo.define
+(
+    'repuestos_stock.repuesto_form', ['web.FormController', 'web.FormView', 'web.view_registry'], function (require) 
+    {
+        'use strict';
 
-    const FormController = require('web.FormController');
-    const FormView = require('web.FormView');
-    const viewRegistry = require('web.view_registry');
+        const FormController = require('web.FormController');
+        const FormView = require('web.FormView');
+        const viewRegistry = require('web.view_registry');
 
-    const RepuestoFormController = FormController.extend({
-        _onFieldChanged: function (event) {
-            this._super.apply(this, arguments);
-            if (event.data.changes.hasOwnProperty('tipo_repuesto')) {
-                const tipo = event.data.changes.tipo_repuesto;
-                const $grupoInyector = this.$('.inyector-section');
+        const RepuestoFormController = FormController.extend({
+            _onFieldChanged: function (event) {
+                this._super.apply(this, arguments);
+                if (event.data.changes.hasOwnProperty('tipo_repuesto')) {
+                    const tipo = event.data.changes.tipo_repuesto;
+                    const $grupoInyector = this.$('.inyector-section');
 
-                if (tipo === 'inyector') {
-                    $grupoInyector.removeClass('o_hidden');
-                } else {
-                    $grupoInyector.addClass('o_hidden');
+                    if (tipo === 'inyector') {
+                        $grupoInyector.removeClass('o_hidden');
+                    } else {
+                        $grupoInyector.addClass('o_hidden');
+                    }
                 }
             }
-        }
-    });
+        });
 
-    const RepuestoFormView = FormView.extend({
-        config: _.extend({}, FormView.prototype.config, {
-            Controller: RepuestoFormController,
-        }),
-    });
+        const RepuestoFormView = FormView.extend({
+            config: _.extend({}, FormView.prototype.config, {
+                Controller: RepuestoFormController,
+            }),
+        });
 
-    viewRegistry.add('repuesto_form_js', RepuestoFormView);
-});
+        viewRegistry.add('repuesto_form_js', RepuestoFormView);
+    }
+);
