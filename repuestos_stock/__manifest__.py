cad2e5fec00da2734fa5f266ad25d5b60f1e4d78
Muyin <burnettnicolas0@gmail.com>
2025-06-25 16:40:26 -0300

    Arreglando el manifest

--- repuestos_stock/__manifest__.py
+++ repuestos_stock/__manifest__.py
@@ -4,15 +4,20 @@
     'category': 'Inventory',
     'summary': 'Gestión personalizada de repuestos para taller y ventas',
     'author': 'Nicolás',
-    'depends': ['product', 'website'],
-    'application': True,
-    'installable': True,
     'license': 'LGPL-3',
+    'depends': [
+        'base',           # aunque base siempre está, ayuda a la claridad
+        'web',            # FormController, FormView, view_registry…
+        'product',        # hereda product.template
+        'point_of_sale',  # define available_in_pos en product.template
+        'website',        # si seguís usando plantillas portal
+    ],
     'data': [
+        'views/assets.xml',         # para fallback si el manifest falla
         'security/ir.model.access.csv',
+        'views/menuitem.xml',
         'views/repuesto_views.xml',
         'views/vehiculo_views.xml',
-        'views/menuitem.xml',
         'templates/portal_repuesto_templates.xml',
         'templates/portal_inyector_template.xml',
     ],
@@ -22,4 +27,6 @@
             'repuestos_stock/static/src/css/repuesto_form.css',
         ],
     },
+    'installable': True,
+    'application': True,
 }
