3825d993e37e8e58a8961f0966b8741fe425308a
Muyin <burnettnicolas0@gmail.com>
2025-06-25 15:26:14 -0300

    Arreglando Js

--- repuestos_stock/static/src/js/repuesto_form.js
+++ repuestos_stock/static/src/js/repuesto_form.js
@@ -10,29 +10,37 @@ odoo.define('repuestos_stock.repuesto_form', [
     const viewRegistry = require('web.view_registry');
 
     const RepuestoFormController = FormController.extend({
-        _onFieldChanged(event) {
-            this._super.apply(this, arguments);
 
-            if (event.data.changes.tipo_repuesto) {
-                const tipo = event.data.changes.tipo_repuesto;
-                const $form = this.$el;
+    _updateSeccionesPorTipo(tipo) {
+        const $form = this.$el;
 
-                console.log('[Repuesto JS] Tipo de repuesto seleccionado:', tipo);
+        $form.find('.grupo-inyector, .grupo-frenos, .grupo-sensor').addClass('o_hidden');
 
-                // Ocultar todas las secciones condicionales
-                $form.find('.grupo-inyector, .grupo-frenos, .grupo-sensor').addClass('o_hidden');
+        if (tipo === 'inyector') {
+            $form.find('.grupo-inyector').removeClass('o_hidden');
+        } else if (tipo === 'pastilla') {
+            $form.find('.grupo-frenos').removeClass('o_hidden');
+        } else if (tipo === 'sensor') {
+            $form.find('.grupo-sensor').removeClass('o_hidden');
+        }
+    },
 
-                // Mostrar la correspondiente al tipo seleccionado
-                if (tipo === 'inyector') {
-                    $form.find('.grupo-inyector').removeClass('o_hidden');
-                } else if (tipo === 'pastilla') {
-                    $form.find('.grupo-frenos').removeClass('o_hidden');
-                } else if (tipo === 'sensor') {
-                    $form.find('.grupo-sensor').removeClass('o_hidden');
-                }
-            }
-        },
-    });
+    _onFieldChanged(event) {
+        this._super.apply(this, arguments);
+        if (event.data.changes.tipo_repuesto !== undefined) {
+            const tipo = event.data.changes.tipo_repuesto;
+            this._updateSeccionesPorTipo(tipo);
+        }
+    },
+
+    _update: function () {
+        return this._super.apply(this, arguments).then(() => {
+            const tipo = this.model.get(this.handle).data.tipo_repuesto;
+            this._updateSeccionesPorTipo(tipo);
+        });
+    },
+});
+
 
     const RepuestoFormView = FormView.extend({
         config: Object.assign({}, FormView.prototype.config, {
