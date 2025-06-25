0177bb35895fd6610c96e672ad60484c10912647
Muyin <burnettnicolas0@gmail.com>
2025-06-25 08:34:12 -0300

    agregando un par de dependencias

--- taller_registro_vehiculo/__manifest__.py
+++ taller_registro_vehiculo/__manifest__.py
@@ -4,7 +4,7 @@
     'summary' : 'Registro de vehiculos por cliente para talleres',
     'category' : 'Taller',
     'author' : 'Burnet Mauricio Nicolas',
-    'depends' : ['base','contacts'],
+    'depends' : ['base','contacts', 'stock', 'sale', 'website'],
     'data' : [
         'security/ir.model.access.csv',
         'views/menuitem.xml',
